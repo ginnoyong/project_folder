fe001d7730299d83926432833da0b75aaf3c26fa
ginnoyong <yongginno@gmail.com>
2024-09-30 16:13:57 +0800

    change about_us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -3,6 +3,6 @@ st.title("About this App")
 st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
 with st.expander(label="How to use this App", expanded=False):
     st.markdown("1. Enter your prompt in the text area.")
-    st.markdown("2. Click the Submit button.")
+    st.markdown("2. Click the Submit button...")
     st.markdown("3. The app will generate a text completion based on your prompt.")
     
